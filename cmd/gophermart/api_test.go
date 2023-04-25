f04bc88a9b3c9a2fb6b635fa62a70efef6ba39f2
Nikita Mramorov <nmramorov98@gmail.com>
2023-04-26 01:24:59 +0300

    Added test for cookies and assert module for testing

--- cmd/gophermart/api_test.go
+++ cmd/gophermart/api_test.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/stretchr/testify/assert"
 )
 
 type MockDb struct {
@@ -304,4 +305,34 @@ func TestAuthentication(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestCookies(t *testing.T) {
+	handler := &Handler{
+		Mux: chi.NewMux(),
+		Cursor: &Cursor{
+			NewMock(),
+		},
+	}
+	handler.Post("/api/user/register", handler.RegisterUser)
+	handler.Post("/api/user/login", handler.Login)
+	ts := httptest.NewServer(handler)
+
+	defer ts.Close()
+
+	buff := bytes.NewBuffer([]byte{})
+	encoder := json.NewEncoder(buff)
+	encoder.Encode(&UserInfo{
+		Username: "test",
+		Password: "test",
+	})
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8080/api/user/register", buff)
+	request.Header.Add("Content-Type", "application/json")
+
+	w := httptest.NewRecorder()
+
+	handler.ServeHTTP(w, request)
+	res := w.Result()
+
+	assert.Equal(t, res.StatusCode, 200)
 }
