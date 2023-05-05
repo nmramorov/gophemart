a9a2dd03afe27f22974010c953809832cd0cf1e6
Nikita Mramorov <nmramorov98@gmail.com>
2023-05-05 18:02:37 +0300

    fixed query

--- cmd/gophermart/queries.go
+++ cmd/gophermart/queries.go
@@ -6,13 +6,13 @@ const (
 	GetOrder              = `SELECT * FROM orders WHERE username=$1 AND _number=$2;`
 	SaveOrder             = `INSERT INTO orders VALUES ($1, $2, $3, $4, $5);`
 	GetOrders             = `SELECT * FROM orders WHERE username=$1;`
-	GetSessionUser        = `SELECT username FROM _session WHERE token=$1;`
+	GetSessionUser        = `SELECT username FROM _sessions WHERE token=$1;`
 	GetBalance            = `SELECT * FROM balances WHERE username=$1;`
 	UpdateBalance  string = `UPDATE balances SET (_current=$1, withdrawn=$2) WHERE username=$3;`
-	GetWithdrawals        = `SELECT * FROM withdrawals WHERE username=$1;`
+	GetWithdrawals        = `SELECT * FROM withdrawal WHERE username=$1;`
 	SaveWithdrawal        = `INSERT INTO withdrawal VALUES ($1, $2, $3, $4);`
 	UpdateOrder           = `UPDATE orders SET (_number=$1, status=$2, accrual=$3) WHERE username=$4;`
-	GetSession            = `SELECT * FROM _session WHERE token=$1;`
+	GetSession            = `SELECT * FROM _sessions WHERE token=$1;`
 	GetAllOrders          = `SELECT * FROM orders;`
 	SaveUserInfo          = `INSERT INTO userinfo VALUES ($1, $2);`
 )
