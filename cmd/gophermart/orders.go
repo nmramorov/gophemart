7734ce3ec37c387c25f9bff862e4148382f08523
Nikita Mramorov <nmramorov98@gmail.com>
2023-05-05 06:22:00 +0300

    hhjj

--- cmd/gophermart/orders.go
+++ cmd/gophermart/orders.go
@@ -81,7 +81,7 @@ func (h *Handler) GetOrders(rw http.ResponseWriter, r *http.Request) {
 	// h.Manager.mu.Lock()
 	// defer h.Manager.mu.Unlock()
 	orders, err := h.Cursor.GetOrders()
-	InfoLog.Println(orders)
+	InfoLog.Println(&orders)
 	// defer h.Manager.mu.Unlock()
 	if err != nil {
 		http.Error(rw, err.Error(), http.StatusInternalServerError)
