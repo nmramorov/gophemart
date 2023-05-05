712987452b0b56e02bd1888b2d8bc4443733f521
Nikita Mramorov <nmramorov98@gmail.com>
2023-05-05 05:45:47 +0300

    fixup

--- cmd/gophermart/orders.go
+++ cmd/gophermart/orders.go
@@ -61,7 +61,7 @@ func (h *Handler) UploadOrder(rw http.ResponseWriter, r *http.Request) {
 	}
 
 	//ToDo: call Accrual Worker to get updates for specific order in goroutine
-	h.Manager.AddJob(order.Number)
+	h.Manager.AddJob(requestNumber)
 
 	if order.Number == requestNumber {
 		rw.WriteHeader(http.StatusOK)
