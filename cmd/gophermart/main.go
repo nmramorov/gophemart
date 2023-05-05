bf677b15fbe4590f6117d7453eb555142a6e12b9
Nikita Mramorov <nmramorov98@gmail.com>
2023-05-05 04:40:07 +0300

    Updated main

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,5 +1,11 @@
 package main
 
 func main() {
-	
+	flags := NewCliOptions()
+	envs, err := NewEnvConfig()
+	if err != nil {
+		ErrorLog.Fatal(err)
+	}
+	app := NewApp(NewConfig(flags, envs))
+	app.Run()
 }
