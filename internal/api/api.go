b7cc2740a8e781d1d6979e58975d6a83f349a7a7
Nikita Mramorov <nmramorov98@gmail.com>
2023-05-11 19:00:08 +0300

    partial fix

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,11 +1,17 @@
 package api
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 
 	"github.com/nmramorov/gophemart/internal/db"
 	"github.com/nmramorov/gophemart/internal/jobmanager"
 )
+
+const REQUEST_TIMEOUT = 60
 
 type Handler struct {
 	*chi.Mux
@@ -21,14 +27,40 @@ func NewHandler(accrualURL string, cursor *db.Cursor) *Handler {
 	}
 	handler.Use(GzipHandle)
 	handler.Use(handler.CookieHandle)
+	handler.Use(middleware.Timeout(REQUEST_TIMEOUT * time.Second))
 
-	handler.Post("/api/user/register", handler.RegisterUser)
-	handler.Post("/api/user/login", handler.Login)
-	handler.Post("/api/user/orders", handler.UploadOrder)
-	handler.Get("/api/user/orders", handler.GetOrders)
-	handler.Get("/api/user/withdrawals", handler.GetWithdrawals)
-	handler.Get("/api/user/balance", handler.GetBalance)
-	handler.Post("/api/user/balance/withdraw", handler.WithdrawMoney)
+	handler.Route("/api/user", func(r chi.Router) {
+		userRouter := NewUserRouter(handler)
+		r.Mount("/", userRouter)
+
+		ordersRouter := NewOrdersRouter(handler)
+		r.Mount("/orders", ordersRouter)
+
+		balanceWithdrawalRouter := NewBalanceWithdrawalsRouter(handler)
+		r.Mount("/", balanceWithdrawalRouter)
+	})
 
 	return handler
+}
+
+func NewUserRouter(handler *Handler) http.Handler {
+	r := chi.NewRouter()
+	r.Post("/register", handler.RegisterUser)
+	r.Post("/login", handler.Login)
+	return r
+}
+
+func NewOrdersRouter(handler *Handler) http.Handler {
+	r := chi.NewRouter()
+	r.Post("/orders", handler.UploadOrder)
+	r.Get("/orders", handler.GetOrders)
+	return r
+}
+
+func NewBalanceWithdrawalsRouter(handler *Handler) http.Handler {
+	r := chi.NewRouter()
+	r.Get("/withdrawals", handler.GetWithdrawals)
+	r.Get("/balance", handler.GetBalance)
+	r.Post("/balance/withdraw", handler.WithdrawMoney)
+	return r
 }
